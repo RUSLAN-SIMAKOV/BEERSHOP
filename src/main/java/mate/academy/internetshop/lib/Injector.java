f624efe8a6a190b32f68ee748265f1f9cc809a54
RUSLAN SIMAKOV <UA667766706@GMAIL.COM>
2020-01-13 22:46:17 +0200

    hw16 3

--- src/main/java/mate/academy/internetshop/lib/Injector.java
+++ src/main/java/mate/academy/internetshop/lib/Injector.java
@@ -7,16 +7,19 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import org.apache.log4j.Logger;
 
 public class Injector {
+
     private static final String PROJECT_MAIN_PACKAGE = "mate.academy.internetshop";
+    final static Logger logger = Logger.getLogger(Injector.class);
     private static List<Class> classes = new ArrayList<>();
 
     static {
         try {
             classes.addAll(getClasses(PROJECT_MAIN_PACKAGE));
         } catch (ClassNotFoundException | IOException e) {
-            e.printStackTrace();
+            logger.error("System failure!", e);;
         }
     }
 
